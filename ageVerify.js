f5a453087f80eba8ba74956b506d23aef77a74fa
apham1594 <88216187+apham1594@users.noreply.github.com>
2021-11-18 15:59:43 -0700

    Made changes to age verify

---
+++ ageVerify.js
@@ -0,0 +1,5 @@
+var yourAge = window.prompt("Please enter your age:")
+    if (yourAge < 21)
+        window.alert("You are" + " " + yourAge + " " +"years old, you must be at least 21 to enter this page!")
+    if (yourAge >= 21) 
+        window.alert("Welcome! You submitted you are" + " " + yourAge + "years old." )
