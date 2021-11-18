4805613a2b517c54c477e147b2f09cf97d9e2ab4
apham1594 <88216187+apham1594@users.noreply.github.com>
2021-11-18 04:13:05 -0700

    made changes

---
+++ Verifier/ageVerify.js
@@ -0,0 +1,7 @@
+var ageCheck = window.prompt("Enter your age:")
+var name = window.prompt("Enter Your name:")
+
+if(ageCheck <18){
+    window.alert("You must be 18 years or older to visit this site, " + name ".")
+    window.alert("Y)
+}
