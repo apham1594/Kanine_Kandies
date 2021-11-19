eb70d1f20542c21c673ee0a3c81b0d6dd35fe68f
apham1594 <88216187+apham1594@users.noreply.github.com>
2021-11-18 20:36:28 -0700

    Age Verify

--- ageVerify.js
+++ ageVerify.js
@@ -3,3 +3,4 @@ var yourAge = window.prompt("Please enter your age:")
         window.alert("You are" + " " + yourAge + " " +"years old, you must be at least 21 to enter this page!")
     if (yourAge >= 21) 
         window.alert("Welcome! You submitted you are" + " " + yourAge + "years old." )
+        
